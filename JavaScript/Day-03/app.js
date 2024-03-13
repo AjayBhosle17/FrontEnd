e822897ea280d8419bc22fd803a19c9ea75a19ed
Ajay Bhosle <ajaybhosle8805@gmail.com>
2024-03-14 01:41:48 +0530

    Object Task

---
+++ JavaScript/Day-03/app.js
@@ -0,0 +1,15 @@
+const profile={
+
+    userName:'ajay_bhosle_17',
+    followers:344,
+    following:400,
+    posts:10,
+    dream:'Businessman',
+    bio:'IT Engineer'
+};
+
+console.log(profile);
+
+console.log(profile.followers);
+
+console.log(profile['posts'])
