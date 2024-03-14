d4405353d2e186717cfa2eec542b1281e9734ee4
Ajay Bhosle <ajaybhosle8805@gmail.com>
2024-03-14 10:50:35 +0530

    User Input

---
+++ JavaScript/Day-04/script.js
@@ -0,0 +1,15 @@
+
+alert("Hey after Today link will be closed");
+
+let name=prompt("Enter Your Name");
+console.log(name);
+
+
+let num=prompt("Enter NUmber")
+
+if(num%5==0){
+
+    console.log(num , " is in table 5")
+}else{
+    console.log(num , " is not table 5")
+}
